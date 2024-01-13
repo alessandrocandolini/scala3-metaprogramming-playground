3531662aab38bc5da00f6e0b9efc7f677c433933
Alessandro Candolini <alessandrocandolini@gmail.com>
2024-01-13 00:20:52 +0000

    format

--- src/main/scala/com/alessandrocandolini/Main.scala
+++ src/main/scala/com/alessandrocandolini/Main.scala
@@ -5,13 +5,13 @@ import com.monovore.decline.Opts
 import com.alessandrocandolini.cli.Args
 import com.alessandrocandolini.utils.CommandIOAppSimple
 
-object Main extends CommandIOAppSimple(
-  name = "scala3-metaprogramming-playground",
-  header = "scala3-metaprogramming-playground",
-  version = "0.1"
-):
+object Main
+    extends CommandIOAppSimple(
+      name = "scala3-metaprogramming-playground",
+      header = "scala3-metaprogramming-playground",
+      version = "0.1"
+    ):
 
   override def run: Opts[IO[Unit]] = Args.readArgs.map(program)
 
-  val program: Args => IO[Unit] = args =>
-    IO.println(s"hello world! $args")
+  val program: Args => IO[Unit] = args => IO.println(s"hello world! $args")
